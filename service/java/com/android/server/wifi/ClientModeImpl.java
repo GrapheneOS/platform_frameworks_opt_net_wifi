37f8bbf1527fc953738b64df71e18b093caa4525
xshu <xshu@google.com>
2020-07-17 01:25:31 -0700

    Do not block BSSID if no internet is expected
    Make the BSSID blocking condition consistent with SSID level blocking.
    
    Bug: 161473332
    Test: atest com.android.server.wifi
    Change-Id: Ieee494069693312d7b7ebbdb292fa9fdf535edb5
    Merged-In: Ieee494069693312d7b7ebbdb292fa9fdf535edb5
    (cherry picked from commit c462c6a2b270227fb76fa5a6a1fab632d3ed08b4)
    (cherry picked from commit ea6fc12902328343c9bb26f6ad56738d6cdcdae8)

--- service/java/com/android/server/wifi/ClientModeImpl.java
+++ service/java/com/android/server/wifi/ClientModeImpl.java
@@ -5416,11 +5416,11 @@ public class ClientModeImpl extends StateMachine {
                                     mWifiConfigManager.updateNetworkSelectionStatus(
                                             config.networkId,
                                             DISABLED_NO_INTERNET_TEMPORARY);
+                                    mBssidBlocklistMonitor.handleBssidConnectionFailure(
+                                            mLastBssid, config.SSID,
+                                            BssidBlocklistMonitor.REASON_NETWORK_VALIDATION_FAILURE,
+                                            mWifiInfo.getRssi());
                                 }
-                                mBssidBlocklistMonitor.handleBssidConnectionFailure(
-                                        mLastBssid, config.SSID,
-                                        BssidBlocklistMonitor.REASON_NETWORK_VALIDATION_FAILURE,
-                                        mWifiInfo.getRssi());
                                 mWifiScoreCard.noteValidationFailure(mWifiInfo);
                             }
                         }
