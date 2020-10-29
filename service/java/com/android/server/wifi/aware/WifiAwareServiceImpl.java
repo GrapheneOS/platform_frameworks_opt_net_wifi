0ae595c7329aa66cf4b10d525de8865e19a4cd44
Steven Moreland <smoreland@google.com>
2020-10-29 19:22:32 +0000

    Revert "[AWARE] API to get available aware resources"
    Revert "[CTS] Add test for AwareResources API."
    
    Revert submission 12903772-awareResources
    
    Reason for revert: b/171991392
    Reverted Changes:
    I74949effc:[CTS] Add test for AwareResources API.
    I8ed29471b:[AWARE] Add API to get available aware resources
    Ie207af078:[AWARE] API to get available aware resources
    
    Change-Id: I88b2b07774cea9f6deda54c9a68a8941c873c428

--- service/java/com/android/server/wifi/aware/WifiAwareServiceImpl.java
+++ service/java/com/android/server/wifi/aware/WifiAwareServiceImpl.java
@@ -24,7 +24,6 @@ import android.app.AppOpsManager;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.wifi.V1_0.NanStatusType;
-import android.net.wifi.aware.AwareResources;
 import android.net.wifi.aware.Characteristics;
 import android.net.wifi.aware.ConfigRequest;
 import android.net.wifi.aware.DiscoverySession;
@@ -158,12 +157,6 @@ public class WifiAwareServiceImpl extends IWifiAwareManager.Stub {
 
         return mStateManager.getCapabilities() == null ? null
                 : mStateManager.getCapabilities().toPublicCharacteristics();
-    }
-
-    @Override
-    public AwareResources getAvailableAwareResources() {
-        enforceAccessPermission();
-        return mStateManager.getAvailableAwareResources();
     }
 
     @Override
