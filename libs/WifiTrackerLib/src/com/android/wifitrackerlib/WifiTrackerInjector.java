fc925238fc7a5c7ebbc1c2c139598ba5211d1c6f
Quang Luong <qal@google.com>
2021-09-29 14:50:16 -0700

    WifiTracker: Do not allow share/easy connect for demo mode
    Devices in demo mode should not be allowed to share/easy connect any
    networks, since this would allow demo users to connect to any
    preconfigured private networks (such as a retail store's network).
    
    Bug: 198625685
    Test: atest WifiTrackerLibTests
    
    Change-Id: I52b7c03f3905e7a38545b814ff90d98cf2879c91
    (cherry picked from commit c80eab12f080939fd4e44cc878c47caff52865f9)
    Merged-In: I4f04c9ba696a905dec1f8def3bf2db7fb2846711

---
+++ libs/WifiTrackerLib/src/com/android/wifitrackerlib/WifiTrackerInjector.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.wifitrackerlib;
+
+import android.content.Context;
+import android.os.UserManager;
+
+import androidx.annotation.NonNull;
+
+/**
+ * Wrapper class for commonly referenced objects and static data.
+ */
+public class WifiTrackerInjector {
+    private final boolean mIsDemoMode;
+
+    // TODO(b/201571677): Migrate the rest of the common objects to WifiTrackerInjector.
+    public WifiTrackerInjector(@NonNull Context context) {
+        mIsDemoMode = ((UserManager) context.getSystemService(Context.USER_SERVICE)).isDemoUser();
+    }
+
+    public boolean isDemoMode() {
+        return mIsDemoMode;
+    }
+}
