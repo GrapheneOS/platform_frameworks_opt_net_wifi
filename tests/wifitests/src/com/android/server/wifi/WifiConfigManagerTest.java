d8635bce99d80b822b78fb296857cfa37aa3f55d
Jimmy Chen <jimmycmchen@google.com>
2022-08-22 15:18:21 +0800

    [DO NOT MERGE] wifi: remove certificates for network factory reset
    When a user does network factory reset, wifi certificate should
    also be cleared as these certificates are also personal credentials.
    
    Bug: 231985227
    Test: atest FrameworksWifiTests
    Merged-In: I0334064347080e43096d01f00cf516804812d0c0
    Change-Id: I0334064347080e43096d01f00cf516804812d0c0

--- tests/wifitests/src/com/android/server/wifi/WifiConfigManagerTest.java
+++ tests/wifitests/src/com/android/server/wifi/WifiConfigManagerTest.java
@@ -691,7 +691,7 @@ public class WifiConfigManagerTest {
         verify(mWcmListener, never()).onSavedNetworkAdded(suggestionNetwork.networkId);
         assertTrue(mWifiConfigManager
                 .removeNetwork(suggestionNetwork.networkId, TEST_CREATOR_UID));
-        verify(mWifiKeyStore, never()).removeKeys(any());
+        verify(mWifiKeyStore, never()).removeKeys(any(), eq(false));
     }
 
     /**
@@ -935,7 +935,7 @@ public class WifiConfigManagerTest {
         assertTrue(mWifiConfigManager.removeNetwork(passpointNetwork.networkId, Process.WIFI_UID));
 
         // Verify keys are not being removed.
-        verify(mWifiKeyStore, never()).removeKeys(any(WifiEnterpriseConfig.class));
+        verify(mWifiKeyStore, never()).removeKeys(any(WifiEnterpriseConfig.class), eq(false));
         verifyNetworkRemoveBroadcast(passpointNetwork);
         // Ensure that the write was not invoked for Passpoint network remove.
         mContextConfigStoreMockOrder.verify(mWifiConfigStore, never()).write(anyBoolean());
@@ -5119,7 +5119,7 @@ public class WifiConfigManagerTest {
         assertTrue(mWifiConfigManager.removeNetwork(configuration.networkId, TEST_CREATOR_UID));
 
         // Verify keys are not being removed.
-        verify(mWifiKeyStore, never()).removeKeys(any(WifiEnterpriseConfig.class));
+        verify(mWifiKeyStore, never()).removeKeys(any(WifiEnterpriseConfig.class), eq(false));
         verifyNetworkRemoveBroadcast(configuration);
         // Ensure that the write was not invoked for Passpoint network remove.
         mContextConfigStoreMockOrder.verify(mWifiConfigStore, never()).write(anyBoolean());
