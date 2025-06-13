2cf0a6f779a8773566aa3e55b5369a59016bede9
Quang Luong <qal@google.com>
2025-06-13 08:22:47 +0000

    [Reupload] Don't show TDI error message if a connection is available
    When a WPA3-Transition or OWE-Transition AP sends out a TDI, the weaker
    security type (WPA2 or Open) is disabled until the user removes the
    network, and a "Unavailable with current security settings" message is
    shown. However, this message is confusing if there is a connectable WPA3
    or OWE AP in range, so avoid showing this message in this case.
    
    This is a reupload of I18d0b49bd5026255da45ac73a1d49b94cd93c617 that
    fixes a NPE.
    
    Flag: EXEMPT minor UI change
    
    Bug: 419014346
    Test: atest StandardWifiEntryTest
    Change-Id: I98d8aeb7f1c50623f272611660334b02495e0009

--- libs/WifiTrackerLib/src/com/android/wifitrackerlib/StandardWifiEntry.java
+++ libs/WifiTrackerLib/src/com/android/wifitrackerlib/StandardWifiEntry.java
@@ -189,8 +189,19 @@ public class StandardWifiEntry extends WifiEntry {
         final @ConnectedState int connectedState = getConnectedState();
         switch (connectedState) {
             case CONNECTED_STATE_DISCONNECTED:
+                // Don't display the TDI error message if we can connect with SAE/OWE.
+                WifiConfiguration configToDescribe = mTargetWifiConfig;
+                if (configToDescribe != null && configToDescribe.getNetworkSelectionStatus()
+                        .getNetworkSelectionDisableReason()
+                        == Utils.DISABLED_TRANSITION_DISABLE_INDICATION) {
+                    for (int upgradeType : List.of(SECURITY_TYPE_SAE, SECURITY_TYPE_OWE)) {
+                        if (!mMatchingScanResults.containsKey(upgradeType)) continue;
+                        if (!mMatchingWifiConfigs.containsKey(upgradeType)) continue;
+                        configToDescribe = mMatchingWifiConfigs.get(upgradeType);
+                    }
+                }
                 connectedStateDescription = getDisconnectedDescription(mInjector, mContext,
-                        mTargetWifiConfig,
+                        configToDescribe,
                         mForSavedNetworksPage,
                         concise);
                 break;
